aaa31315371f2d63bef930751b7e536b873e3b7d
Victor Coutinho <victorkclic@hotmail.com>
2025-09-13 21:14:20 -0300

    Update Dockerfile and AMQP configuration settings

--- service-notifications/config/autoload/amqp.php
+++ service-notifications/config/autoload/amqp.php
@@ -6,17 +6,12 @@ use function Hyperf\Support\env;
 
 return [
     'default' => [
-        'host' => env('RABBITMQ_HOST', 'localhost'),
+        'host' => env('RABBITMQ_HOST', 'rabbitmq'),
         'port' => (int) env('RABBITMQ_PORT', 5672),
-        'user' => env('RABBITMQ_USER', 'guest'),
-        'password' => env('RABBITMQ_PASS', 'guest'),
+        'user' => env('RABBITMQ_USER', 'user'),
+        'password' => env('RABBITMQ_PASS', 'password'),
         'vhost' => env('RABBITMQ_VHOST', '/'),
-        'pool' => [
-            'min_connections' => 1,
-            'max_connections' => 10,
-            'connect_timeout' => 10.0,
-            'wait_timeout' => 3.0,
-            'heartbeat' => 5.0,
-        ],
+        'consumers' => [],
+        'producers' => [],
     ],
 ];
